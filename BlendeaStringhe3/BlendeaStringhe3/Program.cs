48347e4c12e08962115705155c2aab3fc87d29c3
BlendeaMihai <blendeamihail@gmail.com>
2022-02-15 18:48:56 +0100

    Stringhe3

---
+++ BlendeaStringhe3/BlendeaStringhe3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BlendeaStringhe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uguali = 0;
+            string parola = "";
+            while (parola != "fine")
+            {
+                Console.WriteLine("Digita una parola");
+                parola = Console.ReadLine();
+                char letterainiziale = parola[0];
+                int lungh = parola.Length;
+                char letterafinale = parola[lungh - 1];
+                if (letterainiziale == letterafinale)
+                {
+                    uguali++;
+                }
+            }
+            Console.WriteLine($"Le parole che iniziano e finiscono con la stessa lettera sono: {uguali}");
+        }
+    }
+}
