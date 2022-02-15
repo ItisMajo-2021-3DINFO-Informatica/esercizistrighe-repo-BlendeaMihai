18d23f6ea7af5ca6cc42e6bb89bd10fdb910e86e
BlendeaMihai <blendeamihail@gmail.com>
2022-02-15 18:36:46 +0100

    Stringhe5

---
+++ BlendeaStringhe5/BlendeaStringhe5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BlendeaStringhe5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iniziali = 0;
+            Console.WriteLine("Digita un lettera");
+            string lettera = Console.ReadLine();
+            string parola = "";
+            while (parola != "fine")
+            {
+                Console.WriteLine("inserisci una parola");
+                parola = Console.ReadLine();
+                char primalettera = parola[0];
+                if(primalettera== lettera[0])
+                {
+                    iniziali++;
+                }
+
+
+            }
+            Console.WriteLine($"Le parole che iniziano con la lettera {lettera} sono : {iniziali}");
+        }
+    }
+}
